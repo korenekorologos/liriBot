338596517a89d75938cd4c223c67173550aa6ac7
korene korologos <korenekorologos@gmail.com>
2020-01-04 11:04:55 -0700

    node packages have been added

--- liri.js
+++ liri.js
@@ -1,6 +1,17 @@
+/*COMMANDS   
+     concert-this
+     spotify-this-song
+     movie-this
+     do-what-it-says   */
+
+
+     
+//sets enviornmental variables 
 require("dotenv").config();
+
 
 var keys = require("./keys.js");
 
+//spotify 
 var spotify = new Spotify(keys.spotify);
 
